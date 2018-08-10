12cca1c153af9990e21ef163ab802f6fe69102a2
hetao520 <hetao@carexx.cn>
2018-08-10 11:12:24 +0800

    查询订单

--- carexx-uc/src/main/java/com/sh/carexx/uc/manager/CustomerOrderScheduleManager.java
+++ carexx-uc/src/main/java/com/sh/carexx/uc/manager/CustomerOrderScheduleManager.java
@@ -120,31 +120,43 @@ public class CustomerOrderScheduleManager {
                     amountEndDate = DateUtils.addHour(amountStartDate, 12);
                     amountNum += 12;
                     this.doShedule(customerOrderScheduleFormBean, amountStartDate, amountEndDate);
-                }
-                customerOrderSchedule = this.customerOrderScheduleService.getNearByOrderNo(customerOrderScheduleFormBean.getOrderNo());
-                Date startTime = customerOrderSchedule.getServiceStartTime();
-                Date endTime = serviceEndTime;
-                int checkTime = DateUtils.getHourDiff(startTime, endTime);
-                if (checkTime >= 24) {
-                    CustomerOrderSchedule orderSchedule = new CustomerOrderSchedule();
-                    orderSchedule.setId(customerOrderSchedule.getId());
-                    Date time = DateUtils.addHour(customerOrderSchedule.getServiceEndTime(), 12);
-                    orderSchedule.setServiceEndTime(time);
-                    orderSchedule.setServiceDuration(24);
-                    amountEndDate = time;
-                    amountNum += 12;
-                    this.customerOrderScheduleService.updateSchedule(orderSchedule);
-                }
-                while (amountNum < hourNum) {
-                    if (hourNum - amountNum < 24) {
-                        this.doShedule(customerOrderScheduleFormBean, amountEndDate, serviceEndTime);
-                        break;
+                    while (amountNum < hourNum) {
+                        if (hourNum - amountNum < 24) {
+                            this.doShedule(customerOrderScheduleFormBean, amountEndDate, serviceEndTime);
+                            break;
+                        }
+                        amountStartDate = amountEndDate;
+                        amountEndDate = DateUtils.addDay(amountStartDate, 1);
+                        amountNum += 24;
+                        this.doShedule(customerOrderScheduleFormBean, amountStartDate, amountEndDate);
                     }
-                    amountStartDate = amountEndDate;
-                    amountEndDate = DateUtils.addDay(amountStartDate, 1);
-                    amountNum += 24;
-                    this.doShedule(customerOrderScheduleFormBean, amountStartDate, amountEndDate);
+                }else{
+                    customerOrderSchedule = this.customerOrderScheduleService.getNearByOrderNo(customerOrderScheduleFormBean.getOrderNo());
+                    Date startTime = customerOrderSchedule.getServiceStartTime();
+                    Date endTime = serviceEndTime;
+                    int checkTime = DateUtils.getHourDiff(startTime, endTime);
+                    if (checkTime >= 24) {
+                        CustomerOrderSchedule orderSchedule = new CustomerOrderSchedule();
+                        orderSchedule.setId(customerOrderSchedule.getId());
+                        Date time = DateUtils.addHour(customerOrderSchedule.getServiceEndTime(), 12);
+                        orderSchedule.setServiceEndTime(time);
+                        orderSchedule.setServiceDuration(24);
+                        amountEndDate = time;
+                        amountNum += 12;
+                        this.customerOrderScheduleService.updateSchedule(orderSchedule);
+                    }
+                    while (amountNum < hourNum) {
+                        if (hourNum - amountNum < 24) {
+                            this.doShedule(customerOrderScheduleFormBean, amountEndDate, serviceEndTime);
+                            break;
+                        }
+                        amountStartDate = amountEndDate;
+                        amountEndDate = DateUtils.addDay(amountStartDate, 1);
+                        amountNum += 24;
+                        this.doShedule(customerOrderScheduleFormBean, amountStartDate, amountEndDate);
+                    }
                 }
+
             }
         }
     }
