1f47213fb76a9e4cf12a1ea0a0d4a4a4e56901e2
hetao520 <hetao@carexx.cn>
2018-08-14 17:47:13 +0800

    订单金额修改

--- carexx-uc/src/main/java/com/sh/carexx/uc/UCBootstrap.java
+++ carexx-uc/src/main/java/com/sh/carexx/uc/UCBootstrap.java
@@ -4,11 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = { "com.sh.carexx.*" })
 @EnableDiscoveryClient
 @EnableTransactionManagement
+@EnableScheduling
 @MapperScan(basePackages = "com.sh.carexx.uc.dao")
 public class UCBootstrap {
 
