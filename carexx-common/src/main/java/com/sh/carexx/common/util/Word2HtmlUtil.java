5189fbbf624b4fcceda311b0886adbd65c36a552
陈世超 <381627174@qq.com>
2019-04-08 10:13:31 +0800

    统计

--- carexx-common/src/main/java/com/sh/carexx/common/util/Word2HtmlUtil.java
+++ carexx-common/src/main/java/com/sh/carexx/common/util/Word2HtmlUtil.java
@@ -1,8 +1,12 @@
 package com.sh.carexx.common.util;
 
+
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.converter.WordToHtmlConverter;
+import org.apache.poi.xwpf.converter.core.BasicURIResolver;
+import org.apache.poi.xwpf.converter.core.FileImageExtractor;
 import org.apache.poi.xwpf.converter.xhtml.XHTMLConverter;
+import org.apache.poi.xwpf.converter.xhtml.XHTMLOptions;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,9 +20,7 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -38,6 +40,7 @@ public class Word2HtmlUtil {
             httpUrl.connect();
             InputStream in = httpUrl.getInputStream();
             content = Word2HtmlUtil.Word2003ToHtml(in);
+//            content = Word2HtmlUtil.Word2007ToHtml(in);
             return content;
         } catch (IOException e) {
             logger.info("IOException");
@@ -50,18 +53,43 @@ public class Word2HtmlUtil {
         }
     }
 
-    public static String Word2007ToHtml(InputStream in) throws IOException {
-        // 1) 加载word文档生成 XWPFDocument对象
-        XWPFDocument document = new XWPFDocument(in);
-        // 也可以使用字符数组流获取解析的内容
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        XHTMLConverter.getInstance().convert(document, baos, null);
-        String content = baos.toString();
-        baos.close();
-        return content;
+    public static String Word2007ToHtml(InputStream in) throws IOException, ParserConfigurationException, TransformerException {
+//        // 1) 加载word文档生成 XWPFDocument对象
+//        XWPFDocument document = new XWPFDocument(in);
+//        // 也可以使用字符数组流获取解析的内容
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        XHTMLOptions options = XHTMLOptions.create();
+//        XHTMLConverter.getInstance().convert(document, baos, options);
+//        String content = baos.toString();
+//        baos.close();
+//        return content;
+
+        String filepath = "C:\\Users\\hasee\\Desktop";
+        String sourceFileName =filepath+"1.docx";
+        String targetFileName = filepath+"1496717486420.html";
+        String imagePathStr = filepath+"/image/";
+        OutputStreamWriter outputStreamWriter = null;
+        try {
+            XWPFDocument document = new XWPFDocument(in);
+            XHTMLOptions options = XHTMLOptions.create();
+            // 存放图片的文件夹
+            options.setExtractor(new FileImageExtractor(new File(imagePathStr)));
+            // html中图片的路径
+            options.URIResolver(new BasicURIResolver("image"));
+            outputStreamWriter = new OutputStreamWriter(new FileOutputStream(targetFileName), "utf-8");
+            XHTMLConverter xhtmlConverter = (XHTMLConverter) XHTMLConverter.getInstance();
+            xhtmlConverter.convert(document, outputStreamWriter, options);
+        } finally {
+            if (outputStreamWriter != null) {
+                outputStreamWriter.close();
+            }
+        }
+        return "";
     }
 
-    public static String Word2003ToHtml(InputStream in) throws IOException, ParserConfigurationException, TransformerException {
+    public static String Word2003ToHtml(InputStream in) throws
+            IOException, ParserConfigurationException, TransformerException {
+//        XWPFDocument wordDocument2 = new XWPFDocument(in);
         HWPFDocument wordDocument = new HWPFDocument(in);
         WordToHtmlConverter wordToHtmlConverter = new WordToHtmlConverter(
                 DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument());
